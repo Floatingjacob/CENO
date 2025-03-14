ed946ad4bde174baec870994a9c727476a7a45c8
Jacob <108031526+Floatingjacob@users.noreply.github.com>
2025-03-13 21:18:18 -0500

    Add files via upload

--- CENODecoder.py
+++ CENODecoder.py
@@ -1,7 +1,6 @@
-import zipfile
 from PIL import Image
 import base64
-import io
+import os
 Image.MAX_IMAGE_PIXELS = None
 
 binary_color_map = {
@@ -15,14 +14,14 @@ def decode(img):
     pixels = img.load()
     width, height = img.size
     binary_data = []
-    
+
     for y in range(0, height, 5):
         for x in range(0, width, 5):
             color = pixels[x, y]
-            
+
             if color == (255, 255, 255):
                 continue
-                
+
             if color in binary_color_map:
                 binary_data.append(binary_color_map[color])
             else:
@@ -31,22 +30,19 @@ def decode(img):
     binary_data = ''.join(binary_data)
     encoded_data = ''.join(chr(int(binary_data[i:i+8], 2)) for i in range(0, len(binary_data), 8))
     decoded_data = base64.b64decode(encoded_data)
-    
+
     return decoded_data
 
-def decode_zip(zip_file_path, output_file_path):
-    with zipfile.ZipFile(zip_file_path, 'r') as zipf:
-        zipf.extract("color_ceno.png", "/tmp")
-
-    img = Image.open("/tmp/color_ceno.png")
+def decode_file(image_file_path):
+    img = Image.open(image_file_path)
     decoded_data = decode(img)
 
-    with open(output_file_path, "wb") as f:
+    original_filename = os.path.basename(image_file_path).rsplit(".png", 1)[0]
+
+    with open(original_filename, "wb") as f:
         f.write(decoded_data)
 
-    print(f"File has been decoded and saved to: {output_file_path}")
+    print(f"File has been decoded and saved as: {original_filename}")
 
-zip_file_path = input("Enter the path to the ZIP file with CE,NO: ")
-output_file_path = input("Enter the name of the decoded file: ")
-
-decode_zip(zip_file_path, output_file_path)
+image_file_path = input("Enter the path to the encoded PNG file: ")
+decode_file(image_file_path)
